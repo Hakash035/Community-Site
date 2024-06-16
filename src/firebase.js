846bbbb47aebe96533f10ba101843ecde9047cc8
Hakash035 <42975048+Hakash035@users.noreply.github.com>
2024-06-16 09:21:49 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -2,12 +2,7 @@ import { initializeApp } from "firebase/app";
 import {getFirestore} from "firebase/firestore/lite"
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBb4j9tT1vV2XQNSBtD2mLNY9rzfikAvHY",
-  authDomain: "c2c-club.firebaseapp.com",
-  projectId: "c2c-club",
-  storageBucket: "c2c-club.appspot.com",
-  messagingSenderId: "738730871746",
-  appId: "1:738730871746:web:31037555367e190263a45b"
+  // Firebase Config Goes Here
 };
 
 
